9d34182133781f251f11796164a81d0210287919
Malarapu04 <malarapu.bavani@gmail.com>
2021-03-28 17:12:31 -0400

    User Management Code Base

--- src/main/java/com/bhavni/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/bhavni/restservices/SpringbootBuildingblocksApplication.java
@@ -2,8 +2,12 @@ package com.bhavni.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 //Initial Application
-@SpringBootApplication(scanBasePackages = {"com.bhavni.restservices.controller","com.bhavni.restservices.model"})
+@SpringBootApplication(scanBasePackages = {"com.bhavni.restservices.controller","com.bhavni.restservices.model"
+})
+@EntityScan("com.bhavni.restservices.entities")
 public class SpringbootBuildingblocksApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/bhavni/restservices/entities/User.java
@@ -0,0 +1,118 @@
+package com.bhavni.restservices.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Table(name="user")
+public class User {
+
+    @Id
+    @GeneratedValue
+    @Column(name="Employee_ID")
+    private String id;
+
+    @Column(name="USER_NAME",length=50,nullable = false,unique = true)
+    private String username;
+
+    @Column(name="FIRST_NAME")
+    private String firstname;
+
+    @Column(name="LAST_NAME")
+    private String lastname;
+
+    @Column(name="Role")
+    private String role;
+
+    @Column(name="EMAIL")
+    private String email;
+
+    @Column(name="SSN",length=50,nullable = false,unique = true)
+    private String ssn;
+
+/*    public User() {
+    }*/
+/*
+    public User(String id, String username, String firstname, String lastname, String role, String email, String ssn) {
+        this.id = id;
+        this.username = username;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.role = role;
+        this.email = email;
+        this.ssn = ssn;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", role='" + role + '\'' +
+                ", email='" + email + '\'' +
+                ", ssn='" + ssn + '\'' +
+                '}';
+    }*/
+}
